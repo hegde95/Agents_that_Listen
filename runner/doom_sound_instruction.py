1b471228c9a8e6195dff56f887fe7769df5fc808
khegde <kshegde6@gmail.com>
2021-07-03 17:14:05 -0700

    another possible bug fix :/

--- runner/doom_sound_instruction.py
+++ runner/doom_sound_instruction.py
@@ -12,7 +12,7 @@ _experiments = [
         'doom_sound_instruction',
         # 'python -m algorithms.appo.train_appo --algo APPO --train_for_env_steps 100000000 --num_workers 18 --num_envs_per_worker 20 --experiment multisound_hell_vizdoomSoundFFT_3',
         # 'python -m algorithms.appo.train_appo --train_for_env_steps=500000000 --algo=APPO --env_frameskip=4 --use_rnn=True --num_workers=36 --num_envs_per_worker=8 --num_policies=1 --ppo_epochs=1 --rollout=32 --recurrence=32 --batch_size=2048 --wide_aspect_ratio=False',
-        'python -m algorithms.appo.train_appo --train_for_env_steps=500000000 --algo=APPO --env_frameskip=4 --use_rnn=True --num_workers=72 --num_envs_per_worker=8 --num_policies=1 --ppo_epochs=1 --rollout=32 --recurrence=32 --batch_size=2048 --wide_aspect_ratio=False --max_grad_norm=0.0',
+        'python -m rl.train --train_for_env_steps=500000000 --algo=APPO --env_frameskip=4 --use_rnn=True --num_workers=72 --num_envs_per_worker=8 --num_policies=1 --ppo_epochs=1 --rollout=32 --recurrence=32 --batch_size=2048 --wide_aspect_ratio=False --max_grad_norm=0.0',
 
         _params.generate_params(randomize=False),
     ),
