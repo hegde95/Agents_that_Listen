1d82f8932e8403a3b8a72bd2d1037370f4c386a4
khegde <kshegde6@gmail.com>
2021-07-03 17:54:06 -0700

    bug for multiagent

--- envs/doom/multiplayer/doom_multiagent.py
+++ envs/doom/multiplayer/doom_multiagent.py
@@ -146,6 +146,7 @@ class VizdoomEnvMultiplayer(VizdoomEnv):
 
         # added 2 extra line here
         self.game.set_sound_enabled(self.sound_enabled)
+        self.game.set_soft_sound_enabled(self.sound_enabled)
         self.game.set_sound_sampling_freq(self.sampling_freq)
         self.game.set_sound_observation_num_frames(self.number_of_audio_frames)
         
